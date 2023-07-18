5213ce2a24e6c12528b8494792d9df642bd6d1aa
fatima <ftma2011@gmail.com>
2023-07-18 08:45:30 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print_alphabet 10 times.
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char l;
+for (i = 0; i < 10; i++)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
