983ac23679a55368afb8c7b321c62bcd3c85c1fa
fatima <ftma2011@gmail.com>
2023-07-18 10:23:34 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *int _islower -  function prints the alphabet in lowercase
+ *
+ * Return: 1 if it is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return 1; 
+} else
+{
+return 0; 
+}
+}
