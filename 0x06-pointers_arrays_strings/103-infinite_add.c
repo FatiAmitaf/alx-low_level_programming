3726013b4c8dfa3ff209bb01de0d6474fe98e56f
fatima <ftma2011@gmail.com>
2023-07-27 05:58:43 -0700

    task11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,7 @@
  * @n1: string1.
  * @n2: string2.
  * @r: buffer
- * @size_r: buffer size
+ * @size: buffer size
  * Return: String with all letters in ROT13 base.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size)
