18dbe6855c04333d72148f93173f72d5bcbaa2ea
FatiAmitaf <ftma2011@gmail.com>
2023-09-02 04:50:45 -0700

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower -  function prints the alphabet in lowercase
+ *lowercase letter
+ *@c: character to test
+ * Return: 1 if it is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
