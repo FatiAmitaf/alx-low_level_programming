376eaaddaf910ce65e905c0c8b217ccc1d8fc133
FatiAmitaf <ftma2011@gmail.com>
2023-09-02 08:30:09 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * sqrt - This is my my_sqrt_recursion
+ * square - This is my square
  *                     function about the square root
  * @a: The entry is equal to n, of the before function
  * @b: This is the sum
  *
  * Return: This is the result
  */
-int sqrt(int a, int b)
+int square(int a, int b)
 {
 	if (a == (b * b))
 		return (b);
 	else if ((b * b) >= a)
 		return (-1);
 	else
-		return (sqrt(a, b + 1));
+		return (square(a, b + 1));
 }
 
 /**
  * _sqrt_recursion - This is my first function
  * @n: This is my value
  *
- * Return: This is my result of the function my_sqrt_recursion
+ * Return: This is my result of the function square
  */
 int _sqrt_recursion(int n)
 {
 	if (n <= 0)
 		return (-1);
-	return (sqrt(n, 0));
+	return (square(n, 0));
 }
